6a544c3a1dd39b23138c29df35980e28bc38d5d8
cylemon <434520551@qq.com>
2018-01-30 20:34:12 +0800

    spider

---
+++ crawler/baike_spider/html_downloader.py
@@ -0,0 +1,15 @@
+#coding:UTF8
+import urllib2
+
+class HtmlDownloader(object):
+	"""docstring for HtmlDownloader"""
+	def download(self, url):
+		if url is None:
+			return None
+
+		response = urllib2.urlopen(url)
+
+		if response.getcode() != 200:
+			return None
+
+		return response.read()
