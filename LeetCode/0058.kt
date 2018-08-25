c8fae45a54ba6df98ea87a930b82c61a39936e89
Univerce <505772146@qq.com>
2018-08-25 19:28:04 +0700

    Update 0058.kt

--- LeetCode/0058.kt
+++ LeetCode/0058.kt
@@ -1,1 +1,15 @@
-
+class Solution {
+    fun lengthOfLastWord(s: String): Int {
+        var length = 0
+        for (i in s.length - 1 downTo 0) {
+            if(s[i] == ' '){
+                if(length != 0){
+                    return length
+                }
+            } else {
+                length++
+            }
+        }
+        return length
+    }
+}
