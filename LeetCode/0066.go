d5875f444703a823b3fd91b6adcc248c49bcc1d8
Univerce <505772146@qq.com>
2019-03-01 16:08:57 +0800

    Update 0066.go

--- LeetCode/0066.go
+++ LeetCode/0066.go
@@ -1,1 +1,19 @@
-
+func plusOne(digits []int) []int {
+    if len(digits)==0 {
+        return digits
+    }
+    
+    for i := len(digits) -1; i>=0; i-- {
+         if digits[i] < 9 {
+             digits[i]++ //如果是1,2,3这种，直接返回
+             return digits
+         }
+         digits[i] = 0
+    }
+ 
+    length := len(digits)+1
+    res := make([]int, length)
+    
+    res[0] = 1
+    return res
+}
