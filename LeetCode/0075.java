4550f6bdf9716e1854b240a718bcfee2e6a3d627
Univerce <505772146@qq.com>
2019-06-17 18:34:35 +0800

    Update 0075.java

--- LeetCode/0075.java
+++ LeetCode/0075.java
@@ -1,5 +1,5 @@
 Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white and blue.
-
+在计算机科学中，一个原地算法（in-place algorithm）是一种使用小的，固定数量的额外之空间来转换资料的算法。 当算法执行时，输入的资料通常会被要输出的部份覆盖掉。 
 Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
 
 Note: You are not suppose to use the library's sort function for this problem.
@@ -11,6 +11,8 @@ Output: [0,0,1,1,2,2]
 Follow up:
 
 A rather straight forward solution is a two-pass algorithm using counting sort.
+two-pass是一种找连通域的算法，只要对图做两次遍历，就能够对相同连通域内的点打上相同的label。
+计数排序  
 First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
 Could you come up with a one-pass algorithm using only constant space?
   
