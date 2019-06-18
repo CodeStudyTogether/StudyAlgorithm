b164f8b2efc56604698062cf4dc1db3f0a8989fc
Univerce <505772146@qq.com>
2019-06-18 15:23:59 +0800

    Update 0076.java

--- LeetCode/0076.java
+++ LeetCode/0076.java
@@ -1,12 +1,15 @@
 这道题给了我们一个原字符串S，还有一个目标字符串T，让我们在S中找到一个最短的子串，使得其包含了T中的所有的字母，并且限制了时间复杂度为 O(n)。
 https://www.cnblogs.com/springfor/p/3872559.html
+https://blog.csdn.net/zjkC050818/article/details/76098195
 
 class Solution {
     public String minWindow(String S, String T) {
       String res = "";
-      if(S == null || T == null || S.length()==0 || T.length()==0)
+      if(S == null || T == null || S.length()==0 || T.length()==0)//边界情况
           return res;
 
+      //Java语言为内置数据类型char提供了包装类Character类。
+      //// HashMap的key为t中各个字符，value为对应字符个数
       HashMap<Character, Integer> dict = new HashMap<Character, Integer>();
       for(int i =0;i < T.length(); i++){
           if(!dict.containsKey(T.charAt(i)))
