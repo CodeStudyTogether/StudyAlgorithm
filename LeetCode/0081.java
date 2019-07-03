0f7834ca2525c8c2fd5b1c0d2de95d3d9205d287
Univerce <505772146@qq.com>
2019-07-03 18:32:52 +0800

    Update 0081.java

--- LeetCode/0081.java
+++ LeetCode/0081.java
@@ -1,1 +1,11 @@
-
+这一题不按照题者思路写，可以暴力遍历，当然效率很低，不可取
+class Solution {
+    public boolean search(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            if(target == nums[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
