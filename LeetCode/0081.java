dd59ffea04f09979ac27b6dfe502ad38f36ae627
Univerce <505772146@qq.com>
2019-07-03 18:49:31 +0800

    Update 0081.java

--- LeetCode/0081.java
+++ LeetCode/0081.java
@@ -9,3 +9,25 @@ class Solution {
         return false;
     }
 }
+
+https://blog.csdn.net/happyaaaaaaaaaaa/article/details/51602234
+
+排序好的东西找元素，优先考虑二分查找法
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == nums[mid]) return true;
+            if (nums[mid] == nums[left]) left++;
+            else if (nums[mid] > nums[left]) {
+                if (nums[left] <= target && nums[mid] > target) right = mid - 1;
+                else left = mid + 1;
+            } else {
+                if (nums[mid] < target && target <= nums[right]) left = mid + 1;
+                else right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
