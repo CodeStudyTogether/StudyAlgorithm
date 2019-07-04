867f3220298abcdfe8f0f79029a5e97fb3e2f14d
Univerce <505772146@qq.com>
2019-07-04 18:24:15 +0800

    Update 0082.java

--- LeetCode/0082.java
+++ LeetCode/0082.java
@@ -1,1 +1,36 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode helper = new ListNode(0);
+        ListNode pre = helper;
+        boolean isDup = false;
+        while (head != null && head.next != null) {
+            if (head.val == head.next.val) {
+                isDup = true;
+            } else if (isDup) {
+                isDup = false;
+            } else {
+                pre.next = head;
+                pre = pre.next;
+            }
+            head = head.next;
+        }
+        if (!isDup) {
+            pre.next = head;
+            pre = pre.next;
+        } else {
+            pre.next = null;
+        }
+        return helper.next;
+    }
+}
