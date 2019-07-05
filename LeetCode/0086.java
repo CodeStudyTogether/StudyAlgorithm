4f8a55549a4defe36bf7236fc5a7fac11fd24d38
Univerce <505772146@qq.com>
2019-07-05 17:26:59 +0800

    Update 0086.java

--- LeetCode/0086.java
+++ LeetCode/0086.java
@@ -1,1 +1,56 @@
+/**
+https://blog.csdn.net/mine_song/article/details/70545448
 
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    	/**
+	 * 思路是将list按X分成两段
+	 * 小于的连接p
+	 * 大于的连接q
+	 * 最后合并p和q即可
+	 */
+	public ListNode partition(ListNode head, int x) {
+		if (head == null)
+			return head;
+		ListNode p = null;
+		ListNode headP = null;
+		ListNode q = null;
+		ListNode headQ = null;
+		while (head != null) {
+ 
+			if (head.val < x) {
+				if (p == null) {
+					p = head;
+					headP = p;
+				} else {
+					p.next = head;
+					p = p.next;
+				}
+ 
+			} else {
+				if (q == null) {
+					q = head;
+					headQ = q;
+				} else {
+					q.next = head;
+					q = q.next;
+				}
+			}
+			head = head.next;
+		}
+		if (q != null)
+			q.next = null;
+		if (p == null)
+			return headQ;
+ 
+		p.next = headQ;
+		return headP;
+ 
+	}
+}
