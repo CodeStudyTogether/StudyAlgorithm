4db113470f3360f2058c07137fd49d933eaa1cbc
Univerce <505772146@qq.com>
2019-07-05 10:53:42 +0800

    Update 0083.java

--- LeetCode/0083.java
+++ LeetCode/0083.java
@@ -1,1 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode pre = head;
+        while(pre != null && pre.next != null){
+            if(pre.val == pre.next.val){
+                pre.next = pre.next.next;
+            } else {
+                pre = pre.next;
+            }
+        }
+        return head;
+    }
+}
 
+other's solution:
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+   public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null)return head;
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
+}
