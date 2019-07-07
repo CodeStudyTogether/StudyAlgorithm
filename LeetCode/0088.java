37b3341b659e1d45c027f6204e32ed4ca3422e0e
Univerce <505772146@qq.com>
2019-07-07 15:05:29 +0800

    Update 0088.java

--- LeetCode/0088.java
+++ LeetCode/0088.java
@@ -1,1 +1,34 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = 0; i < m+n; i++){
+            if(i >= m){
+                nums1[i] = nums2[i-m];
+            }
+        }
+        Arrays.sort(nums1);
+    }
+}
 
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = m + n - 1;
+		int i = m - 1;
+		int j = n - 1;
+ 
+		while (k >= 0) {
+			if (i < 0) {
+				nums1[k--] = nums2[j--];
+				continue;
+			}
+			if (j < 0) {
+				break;
+			}
+ 
+			if (nums1[i] > nums2[j]) {
+				nums1[k--] = nums1[i--];
+			} else {
+				nums1[k--] = nums2[j--];
+			}
+		}
+    }
+}
