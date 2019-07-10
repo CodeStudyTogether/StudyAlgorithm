2d84311315eb6a9223d455df228eeaa8d0c15a31
Univerce <505772146@qq.com>
2019-07-10 15:21:10 +0800

    Update 0100.java

--- LeetCode/0100.java
+++ LeetCode/0100.java
@@ -1,1 +1,19 @@
-
+    /**
+     * 搞清楚二叉树的基本遍历方式，递归乃是核心中的核心。
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null & q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val == q.val) {//递归啊递归
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
