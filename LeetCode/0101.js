c61fda782626dcc036d0431c69d3652a610a7143
Univerce <505772146@qq.com>
2019-08-12 16:03:48 +0800

    Update 0101.js

--- LeetCode/0101.js
+++ LeetCode/0101.js
@@ -1,1 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ * 使用递归法（Recursive）解决。
+ */
+var isSame = function (left, right) {
+    if (!left && !right) return true;
+    if (!left || !right || right.val !== left.val) return false;
+    return isSame(left.left, right.right) && isSame(left.right, right.left);//逐级比较
+};
 
+var isSymmetric = function(root) {
+    if (!root) return true;
+    return isSame(root.left, root.right);//从根目录开始遍历
+};
