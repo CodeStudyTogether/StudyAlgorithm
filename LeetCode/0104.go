0b85adfd65d590cf8ef0135037d62ae1dda37144
Univerce <505772146@qq.com>
2022-04-19 15:32:06 +0800

    Update 0104.go

--- LeetCode/0104.go
+++ LeetCode/0104.go
@@ -1,1 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    leftDepth := maxDepth(root.Left)
+    rightDepth := maxDepth(root.Right)
+    if leftDepth >= rightDepth {
+        return 1 + leftDepth
+    }
+    return 1 + rightDepth
+}
 
+//我们的思路是对于求整棵树的深度，就是根节点的左子树的深度或者根结点的右子树的深度加1，那么，使用递归的方式就是
+//depth(root)=1+max(depth(root.Left),depth(root.Right))
